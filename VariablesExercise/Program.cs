21b52e0db620d31880b3ac08364c3ef71349e0d0
darlene503 <duyn503@gmail.com>
2023-07-26 16:03:41 -0700

    repush

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,33 @@
 ﻿namespace VariablesExercise
 {
+
     public class Program
     {
+
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello, World!");
+            string dogName = "Sir Dudley";
+            int age = 9;
+            char toys = '4';
+            bool isUnder3 = false;
+            double currentCostofDogfood = 42.99;
+            decimal currentPriceofVetvisit = 52.50m;
+
+            Console.WriteLine($"Dog Name:{dogName}");
+            Console.WriteLine($"Age:{age}");
+            Console.WriteLine($"Toys:{toys}");
+            Console.WriteLine($"Is under 3:{isUnder3}");
+            Console.WriteLine($"Current Cost Of dog food:{currentCostofDogfood}");
+            Console.WriteLine($"Current Cost Of vet visit:{currentPriceofVetvisit}");
+
+
+        }
+
+        private string GetDebuggerDisplay()
+        {
+            return ToString();
         }
     }
+
 }
